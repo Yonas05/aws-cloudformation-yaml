67b259c607ca613b63724df26b379cd00d073fde
Yonas Fayera <yonasfayera05@gmail.com>
2026-02-23 15:02:24 +0300

    private instance addition

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ resource "aws_instance" "app_server" {
     }
 
 }
+resource "aws_instance" "Private_instance" {
+  ami           = "ami-0f3caa1cf4417e51b"
+  instance_type = var.instance_private_type
+    tags = {
+        Name = var.private_instance    
+    }
+  
+}
