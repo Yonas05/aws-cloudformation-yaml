44f0300f88e582fd0e385bb575be618de5f38ea3
Yonas Fayera <yonasfayera05@gmail.com>
2026-02-23 16:08:36 +0300

    destory terrafom

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "instance_name" {
-  description = "The name of the EC2 instance."
-  value       = var.instance_name
-}
-output "instance_type" {
-  description = "The type of the EC2 instance."
-  value       = var.instance_type
+# output "instance_name" {
+#   description = "The name of the EC2 instance."
+#   value       = var.instance_name
+# }
+# output "instance_type" {
+#   description = "The type of the EC2 instance."
+#   value       = var.instance_type
 
-}
+# }
