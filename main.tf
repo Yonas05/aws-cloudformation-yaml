f5f6a75b0df81a9f9808510098c476e59d4e1990
Yonas Fayera <yonasfayera05@gmail.com>
2026-02-23 16:08:21 +0300

    destory terrafom

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 provider "aws" {
   region = "us-east-1"
-  
 }
-resource "aws_instance" "app_server" {
-  ami           = "ami-0f3caa1cf4417e51b"
-  instance_type = var.instance_type
-    tags = {
-        Name = var.instance_name    
-    }
+# }
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-0f3caa1cf4417e51b"
+#   instance_type = var.instance_type
+#     tags = {
+#         Name = var.instance_name    
+#     }
 
-}
-resource "aws_instance" "Private_instance" {
-  ami           = "ami-0f3caa1cf4417e51b"
-  instance_type = var.instance_private_type
-    tags = {
-        Name = var.private_instance    
-    }
+# }
+# resource "aws_instance" "Private_instance" {
+#   ami           = "ami-0f3caa1cf4417e51b"
+#   instance_type = var.instance_private_type
+#     tags = {
+#         Name = var.private_instance    
+#     }
   
-}
+# }
