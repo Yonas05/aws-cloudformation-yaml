9c1b5319fc5cdef25b4bd54ba5db5f7863ede921
Yonas Fayera <yonasfayera05@gmail.com>
2026-02-26 15:17:48 +0300

    sets up IAM roles and permissions for EC2 instances to use AWS Systems Manager (SSM)

--- main.tf
+++ main.tf
@@ -90,20 +90,73 @@ resource "aws_security_group" "vpc_b_sg" {
 }
 
 resource "aws_security_group_rule" "allow_b_to_a" {
-  type = "ingress"
-  from_port = 22
-  to_port = 22
-  protocol = "tcp"
-  security_group_id = aws_security_group.vpc_a_sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.vpc_a_sg.id
   source_security_group_id = aws_security_group.vpc_b_sg.id
-  
+
 }
 resource "aws_security_group_rule" "allow_a_to_b" {
-  type = "ingress"
-  from_port = 22
-  to_port = 22
-  protocol = "tcp"
-  security_group_id = aws_security_group.vpc_b_sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.vpc_b_sg.id
   source_security_group_id = aws_security_group.vpc_a_sg.id
-  
+
+}
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm_attach" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "ec2-ssm-instance-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "ec2_a" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.demo-subnet-a.id
+  vpc_security_group_ids = [aws_security_group.sg_vpc_a.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_profile.name
+  tags                   = { Name = "ec2-vpc-a" }
+}
+
+resource "aws_instance" "ec2_b" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.demo-subnet-b.id
+  vpc_security_group_ids = [aws_security_group.sg_vpc_b.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_profile.name
+  tags                   = { Name = "ec2-vpc-b" }
 }
